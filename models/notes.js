f8da2e4e2726fc16b63643c1eab51e60197e8757
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-08-20 08:18:27 +0200

    "Added 'category' and 'date' fields to noteSchema"

--- models/notes.js
+++ models/notes.js
@@ -9,6 +9,14 @@ const noteSchema = new Schema({
     content: {
         type: String,
         required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
     }
 });
 
