be601fa352151bd3fd3ce671ce057cec45c7c5c8
Clever Designer <107514372+clvrdesign@users.noreply.github.com>
2024-08-20 08:04:33 +0200

    "Added catch-all route to return 'Welcome to the API' message"

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(express.json())
 // Routes
 app.use('/users', users)
 app.use('/notes', notes)
+app.get('/*', (req, res)=>{
+    res.json({message:'Welcome to the API'})
+})
 
 connect(process.env.MONGO_URI)
     .then(()=>{
